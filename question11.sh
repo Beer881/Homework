5bdd72b52cd63c87854402961e5fff316fddc945
beer <126881050+Beer881@users.noreply.github.com>
2023-03-19 11:29:54 +0000

    q11

--- question11.sh
+++ question11.sh
@@ -2,7 +2,7 @@ git checkout -b branch2
 touch file4
 git add file4
 git commit -m "commit message"
-cat "new content" > file4
+echo "new content" > file4
 git add file4
 git stash
 git checkout main
